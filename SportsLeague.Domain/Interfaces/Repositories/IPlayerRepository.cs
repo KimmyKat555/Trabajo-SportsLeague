658448dcfc03402da65f711004cae8252a277f76
KimmyKat555 <kimberly234@outlook.es>
2026-04-09 17:28:14 -0500

    fase 2 primera parte

---
+++ SportsLeague.Domain/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface IPlayerRepository : IGenericRepository<Player>
+    {
+        Task<IEnumerable<Player>> GetByTeamAsync(int teamId);
+        Task<Player?> GetByTeamAndNumberAsync(int teamId, int number);
+        Task<IEnumerable<Player>> GetAllWithTeamAsync();
+        Task<Player?> GetByIdWithTeamAsync(int id);
+    }
+}
