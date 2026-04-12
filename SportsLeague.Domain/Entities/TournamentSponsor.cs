8a9009a983c439ae176ccdcd41045063058812d4
KimmyKat555 <kimberly234@outlook.es>
2026-04-12 14:33:48 -0500

    final correcto

--- SportsLeague.Domain/Entities/TournamentSponsor.cs
+++ SportsLeague.Domain/Entities/TournamentSponsor.cs
@@ -1,5 +1,6 @@
-﻿namespace SportsLeague.Domain.Entities
-{
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace SportsLeague.Domain.Entities;
+
     public class TournamentSponsor : AuditBase
     {
         public int TournamentId { get; set; }
@@ -8,6 +9,7 @@
         public int SponsorId { get; set; }
         public virtual Sponsor Sponsor { get; set; } = null!;
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal ContractAmount { get; set; }
     }
-}
+
