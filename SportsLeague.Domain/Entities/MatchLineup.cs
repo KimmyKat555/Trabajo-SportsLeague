c42fb6d1ec420ba052d9070d78ffd91d687aedd5
KimmyKat555 <kimberly234@outlook.es>
2026-05-22 15:15:48 -0500

    entrega 4 match lineup y dtos

---
+++ SportsLeague.Domain/Entities/MatchLineup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class MatchLineup : AuditBase
+    {
+        public new int Id { get; set; }
+        public int MatchId { get; set; }
+        public virtual Match Match { get; set; } = null!;
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; } = null!;
+        public bool IsStarter { get; set; }
+        public string Position { get; set; } = null!; 
+    }
+}
---
+++ SportsLeague.Domain/Interfaces/Repositories/IMatchLineupRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface IMatchLineupRepository : IGenericRepository<MatchLineup>
+    {
+        Task<IEnumerable<MatchLineup>> GetByMatchAsync(int matchId);
+        Task<IEnumerable<MatchLineup>> GetByTeamAsync(int matchId, int teamId);
+        Task<int> CountStartersAsync(int matchId, int teamId);
+        Task<MatchLineup> AddAsync(MatchLineup lineup);
+        Task DeleteAsync(MatchLineup lineup);
+    }
+}
