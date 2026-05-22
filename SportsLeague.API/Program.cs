7e26de0c2fe7d5fb152f69369cf7bebdfa12222c
KimmyKat555 <kimberly234@outlook.es>
2026-05-21 20:21:38 -0500

    fase 6

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddScoped<ISponsorService, SponsorService>();
 builder.Services.AddScoped<IMatchService, MatchService>();
 builder.Services.AddScoped<IMatchEventService, MatchEventService>();
 builder.Services.AddScoped<MatchValidationHelper>();
+builder.Services.AddScoped<IStandingsService, StandingsService>();
 
 // AutoMapper 
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
